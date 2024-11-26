afd9564e2a1dd36170b3a0b8141bcad4f9ea5a86
N. <narenkdk@gmail.com>
2024-11-26 17:45:59 +0545

    Update Day 1.py

--- Day 1.py
+++ Day 1.py
@@ -1,5 +1,8 @@
 #Day 1 - Hello World
 
+#Q : 1. Write a program that prints "Hello World!" to the console
+#2. Add single line and multi-line comments to your code
+
 print("Hello World!")
 
 #day oen of one hundred days of python coding 
