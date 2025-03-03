d1de7b7514b96e5bc1e94b66318ec3da93a17798
Naren Khadka <narenkdk@gmail.com>
2025-03-03 10:53:58 +0545

    Add files via upload

---
+++ Day 100.py
@@ -0,0 +1,18 @@
+#Day 100: Portfolio
+
+
+#Build a portfolio of Python projects to showcase your skills.
+
+#This is the last day of the 100 days of code challenge. I have learned a lot and I am happy to have completed this challenge. 
+# I will continue to learn and improve my skills. I will also continue to work on projects and build my portfolio. 
+
+#For my portfolio, I will include projects that I've worked in during the 100 days of code challenge. 
+#Here is the link to my github repository where I have uploaded all the projects:
+
+#https://github.com/narenkdk
+
+#Would be happy to hear feedback on my projects.
+
+
+
+#Thank you for following along with me on this journey. Do check "https://www.100daysofcode.io/" for more information on the challenge.
